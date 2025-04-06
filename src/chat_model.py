c0113ab075925f41e260be0a63f0c47be506855c
Denis Shafarenko <phisika001@mail.ru>
2025-04-06 21:26:15 +0300

    start building mvc architecture

--- src/chat_model.py
+++ src/chat_model.py
@@ -10,10 +10,11 @@ class ChatAssistant:
     def add_assistant_message(self, message):
         self.chat_history.append({'role': 'assistant', 'content': message})
 
-    def get_response(self, stream=False):
+    def generate_response(self, stream=False):
         response = self.llm.create_chat_completion(
             messages=self.chat_history,
             temperature=self.temperature,
             stream=stream
         )
+
         return response
